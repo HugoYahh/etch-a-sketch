ed620806518b5ab9ac7badf2c5fa283d62f2ec43
HugoYah <clauzoh@gmail.com>
2025-07-07 19:13:55 +0200

    Add the grid, and style it, add comments

--- script.js
+++ script.js
@@ -11,21 +11,28 @@ let gridSizeText = document.querySelector("#grid-size-text")
 /* On change, recreate the grid */
 inputSize.addEventListener("change", createGrid)
 
+/*On load, create a grid*/
+window.addEventListener("load", (e) => {
+    createGrid()
+})
+
 /*WHILE changing, update the textContent for the user to see */
 inputSize.addEventListener("input", (event) => {
     gridSizeText.textContent = event.target.value + "x" + event.target.value;
 })
 
 /*Creating the div grid, styling it */
-function createGrid(e) {
-    gridContainer.innerHTML = ""; /*If exists, deletes ancient divs from another grid*/
-    for (let i = 1; i <= document.querySelector("#grid-size").value; i++) {
-        let divi = document.createElement("div");
-        gridContainer.appendChild(divi);
-        for (let j = 1; j <= document.querySelector("#grid-size").value; j++) {
-            let divj = document.createElement("div")
-            divi.appendChild(divj)
-        }
-    }
 
+
+function createGrid(e) {
+    gridContainer.innerHTML = "";
+    for (let i = 1; i <= document.querySelector("#grid-size").value * document.querySelector("#grid-size").value; i++) {
+        let divi = document.createElement("div");
+        divi.classList.add("grid")
+        let width = 500 / Math.floor(parseInt(document.querySelector("#grid-size").value))
+        divi.style.height = width + "px"
+        divi.style.width = width + "px"
+        divi.style.backgroundColor = "white"
+        gridContainer.appendChild(divi);
+    }
 }
