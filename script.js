4c437fe62956668376a7c3187ab8734ecc2e65cf
HugoYah <clauzoh@gmail.com>
2025-07-08 20:08:03 +0200

    FInal version of etch a sketch !

--- script.js
+++ script.js
@@ -8,6 +8,37 @@ let gridContainer = document.querySelector(".grid-container")
 let label = document.querySelector("label")
 let gridSizeText = document.querySelector("#grid-size-text")
 
+let isMouseDown = false;
+
+const MODES = {
+    DRAW: 'draw',
+    ERASER: 'eraser',
+    RAINBOW: 'rainbow',
+    CLEAR: 'clear',
+    // Add more modes as needed, e.g., 'fill', 'picker'
+};
+
+let currentMode = MODES.DRAW;
+
+btnEraser.addEventListener("click", () => {
+    currentMode = MODES.ERASER;
+})
+
+btnRainbow.addEventListener("click", () => {
+    currentMode = MODES.RAINBOW;
+})
+
+btnColor.addEventListener("click", () => {
+    currentMode = MODES.DRAW;
+})
+
+btnClear.addEventListener("click", () => {
+    document.querySelectorAll(".grid").forEach((element) => {
+        element.style.backgroundColor = "white";
+    })
+    console.log("clear clicked")
+})
+
 /* On change, recreate the grid */
 inputSize.addEventListener("change", createGrid)
 
@@ -16,11 +47,64 @@ window.addEventListener("load", (e) => {
     createGrid()
 })
 
+
+
 /*WHILE changing, update the textContent for the user to see */
 inputSize.addEventListener("input", (event) => {
     gridSizeText.textContent = event.target.value + "x" + event.target.value;
 })
 
+
+gridContainer.addEventListener("mousedown", (event) => {
+    isMouseDown = true;
+
+
+})
+
+document.addEventListener("mouseup", (event) => {
+    isMouseDown = false;
+
+})
+
+let currentSquare = null
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (isMouseDown) {
+        const target = e.target
+        if (currentSquare !== target) {
+            currentSquare = target;
+            paintSquare(currentSquare)
+        }
+    }
+})
+
+function paintSquare(currentSquare) {
+    let color = document.querySelector(".color-choice").value
+    switch (currentMode) {
+        case MODES.DRAW:
+            currentSquare.style.backgroundColor = color;
+            break;
+        case MODES.ERASER:
+            currentSquare.style.backgroundColor = "white";
+            break;
+        case MODES.RAINBOW:
+            currentSquare.style.backgroundColor = RainbowColorChoice();
+            break;
+        default:
+            console.log("ERROR")
+            break;
+    }
+
+}
+
+function RainbowColorChoice() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 /*Creating the div grid, styling it */
 
 
@@ -36,3 +120,5 @@ function createGrid(e) {
         gridContainer.appendChild(divi);
     }
 }
+
+
